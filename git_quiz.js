c0b006c8a9302048245a11a0dd9a3e0693832c3a
Saif <92186507+Saif-C@users.noreply.github.com>
2021-11-19 14:33:52 -0800

    Add files via upload

---
+++ git_quiz.js
@@ -0,0 +1,25 @@
+function buttonfunc(){
+    var textBox1 = document.getElementById("one").value;
+    console.log(textBox1);
+    
+    var textBox2 = document.getElementById("q3").value;
+	console.log(textBox2);
+
+    var op3 = document.querySelector("#op3").checked;
+    console.log(op3);
+    
+    var op4 = document.querySelector("#op4").checked;
+    console.log(op4);
+    
+    var op7 = document.querySelector("#op7").checked;
+    console.log(op7);
+    
+    var op11 = document.querySelector("#op11").checked;
+    console.log(op11);
+
+    var radio1 = document.querySelector("#pglanguage").checked;
+	console.log(radio1);
+	
+    var radio2 = document.querySelector("#Mlanguage").checked;
+	console.log(radio2);   
+}
